05a1cbdfef5cbbd88dbd08a60e916c246144f66d
Jean28518 <jf_v@gmx.de>
2022-08-24 14:16:56 +0200

    Set default page

--- src/event_management_system/event_management_system/urls.py
+++ src/event_management_system/event_management_system/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 
 urlpatterns = [
+    path('', views.index, name="event_management_system_index"),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('events/', include('events.urls')),
