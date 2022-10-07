81a2db8368d832a69c6e5603018f1fdb5fde52fe
Jean28518 <jf_v@gmx.de>
2022-10-07 20:53:33 +0200

    Fix automatic login

--- src/event_management_system/event_management_system/views.py
+++ src/event_management_system/event_management_system/views.py
@@ -9,6 +9,8 @@ def index(request):
     set_rights.init()
     if User.objects.count() == 0:
         return HttpResponseRedirect("/users/register/?next=/")
+    if not request.user.is_authenticated:
+        return redirect('user_login')
     if request.user.groups.filter(name='Contact').exists():
         return redirect('lecture_contact_overview')
     return redirect('event_overview')
