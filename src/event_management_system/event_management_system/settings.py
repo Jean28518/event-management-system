d0eb626e61b3f99fa55bb48c3e599aae081b3613
Jean3219 <jf_v@gmx.de>
2022-10-10 14:42:53 +0200

    Update settings.py

--- src/event_management_system/event_management_system/settings.py
+++ src/event_management_system/event_management_system/settings.py
@@ -151,7 +151,8 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")                    # <- host name [e.g. smt
 EMAIL_PORT = int(os.getenv("EMAIL_PORT"))               # <- smtp port [e.g. 587]
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")          # <- username
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # <- password
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
 
 
 LOGIN_URL = "/users/login"
