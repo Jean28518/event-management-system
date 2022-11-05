e5692ba0d02683b5151a653e85f75b38ca18c175
Jean28518 <jf_v@gmx.de>
2022-11-05 21:56:34 +0100

    improve timetable view

--- src/event_management_system/users/forms.py
+++ src/event_management_system/users/forms.py
@@ -12,7 +12,7 @@ ROLES = [
 class UserForm(forms.Form):
     user_role= forms.CharField(label=_('Role'), widget=forms.Select(choices=ROLES, attrs={'class': "form-control"}))
     email = forms.CharField(label=_('Email'), max_length=100, widget=forms.EmailInput(attrs={'class': "form-control"}))
-    first_name = forms.CharField(label=_('First name'), max_length=100, widget=forms.TextInput(attrs={'class': "form-control"}),)
+    first_name = forms.CharField(label=_('First name'), max_length=100, widget=forms.TextInput(attrs={'class': "form-control"}), required=False)
     last_name = forms.CharField(label=_('Last name'), max_length=100, widget=forms.TextInput(attrs={'class': "form-control"}),)
     password = forms.CharField(label=_('Password'), max_length=100, widget=forms.TextInput(attrs={'class': "form-control"}),)
     website = forms.CharField(label=_('Website'), max_length=100, widget=forms.URLInput(attrs={'class': "form-control"}), required=False)
