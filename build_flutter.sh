0965c6d2e0cf1886477dbf9d93c985bd048c7e9e
Jean28518 <jf_v@gmx.de>
2023-01-19 23:07:35 +0100

    Import ems_timetable to static files

--- build_flutter.sh
+++ build_flutter.sh
@@ -3,4 +3,4 @@ PATH="$PATH:`pwd`/flutter/bin"
 flutter doctor
 cd src/flutter/ems_timetable/
 flutter build web --base-href "/static/ems_timetable/"
-cp -r build/web ../../event_management_system/ems_timetable
+cp -r build/web ../../event_management_system/static/ems_timetable
--- src/event_management_system/event_management_system/settings.py
+++ src/event_management_system/event_management_system/settings.py
@@ -141,6 +141,10 @@ LOCALE_PATHS = [
 STATIC_ROOT = '/static/'
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
